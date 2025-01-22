681560451cc8baa7a09328d3592c63634b9dc111
Janhavi Khonde <janhavikhonde20@gmail.com>
2025-01-22 15:59:05 +0530

    Fixing 404 error

--- server.js
+++ server.js
@@ -2,17 +2,22 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Route to serve the main HTML file
+// Serve the main index.html
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+// Handle 404 errors
+app.use((req, res) => {
+    res.status(404).send('404: NOT_FOUND');
+});
+
 // Start the server
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+    console.log(`Server running at http://localhost:${port}`);
 });
